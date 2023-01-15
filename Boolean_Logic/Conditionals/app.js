1b1815cbb25c6567ba08511ba50577f6dd50db4f
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-01-15 16:06:24 +0900

    else if statement

--- Boolean_Logic/Conditionals/app.js
+++ Boolean_Logic/Conditionals/app.js
@@ -1,9 +1,29 @@
-let random = Math.random();
-if (random < 0.5) {
-    console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
-    console.log(random);
-}
-if (random >= 0.5) {
-    console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
-    console.log(random);
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
+//     console.log(random);
+// }
+// if (random >= 0.5) {
+//     console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
+//     console.log(random);
+// }
+
+// const dayOfWeek = "Friday";
+
+// if (dayOfWeek === "Monday") {
+//     console.log("UGHHH I HATE MONDAYS!");
+// } else if (dayOfWeek === "Saturday") {
+//     console.log("YAY I LOVE SATURDAYS!");
+// } else if (dayOfWeek === "Friday") {
+//     console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!");
+// }
+
+const age = 8;
+
+if (age < 5) {
+    console.log("Your are a baby. You get in for free!");
+} else if (age < 10) {
+    console.log("You are a child. You pay $10");
+} else if (age < 65) {
+    console.log("You are an adult. You pay $20 ");
 }

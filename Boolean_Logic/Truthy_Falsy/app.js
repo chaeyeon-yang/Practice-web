fa8a8ede5d27e5623ecd4fd66803c8d4b7956e6c
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-01-15 16:33:01 +0900

    Truth-y&False-y Values

---
+++ Boolean_Logic/Truthy_Falsy/app.js
@@ -0,0 +1,38 @@
+// const userInput = prompt("Enter something");
+
+// if (userInput) {
+//     console.log("TRUTHY!");
+// } else {
+//     console.log("FALSY!");
+// }
+if (0) {
+    console.log("TRUTHY");
+} else {
+    console.log("FALSY");
+}
+
+if (null) {
+    console.log("TRUTHY");
+} else {
+    console.log("FALSY");
+}
+
+if (NaN) {
+    console.log("TRUTHY");
+} else {
+    console.log("FALSY");
+}
+
+if (undefined) {
+    console.log("TRUTHY");
+} else {
+    console.log("FALSY");
+}
+
+// Falsy values
+// fasle
+// 0
+// ""(empty string)
+// null
+// undefined
+// NaN

6b3df5092a42251cc8ff18ab4cf7c03e3718a130
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-01-15 17:22:20 +0900

    switch

--- Boolean_Logic/Logical_Operators/app.js
+++ Boolean_Logic/Logical_Operators/app.js
@@ -28,7 +28,32 @@
 //     firstName = prompt("TRY AGAIN!!!");
 // }
 
-const age = 45;
-if (!((age >= 0 && age < 5) || age >= 65)) {
-    console.log("YOU ARE NOT A BABY OR A SENIOR!");
+// const age = 45;
+// if (!((age >= 0 && age < 5) || age >= 65)) {
+//     console.log("YOU ARE NOT A BABY OR A SENIOR!");
+// }
+
+const day = 2;
+switch (day) {
+    case 1:
+        console.log("MONDAY");
+        break;
+    case 2:
+        console.log("TUESDAY");
+        break;
+    case 3:
+        console.log("WEDNESDAY");
+        break;
+    case 4:
+        console.log("THURSDAY");
+        break;
+    case 5:
+        console.log("FRIDAY");
+        break;
+    case 6:
+    case 7:
+        console.log("WEEKEND!");
+        break;
+    default:
+        console.log("I DONT KNOW THAT!");
 }

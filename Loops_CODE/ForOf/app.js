f7adf5a76ae7429f691b93860b6ecbefc3d5d426
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-01-19 15:00:09 +0900

    For..Of Loop

---
+++ Loops_CODE/ForOf/app.js
@@ -0,0 +1,32 @@
+// const subreddits = [
+//     "cringe",
+//     "books",
+//     "chickens",
+//     "funny",
+//     "pics",
+//     "soccer",
+//     "gunners",
+// ];
+
+// const seatingChart = [
+//     ["Kristen", "Erik", "Namita"],
+//     ["Geoffrey", "Juanita", "Antonio", "Kevin"],
+//     ["Yuma", "Sakura", "Jack", "Erika"],
+// ];
+// for (let i = 0; i < subreddits.length; i++) {
+//     console.log(`Visit reddit.com/r/${subreddits[i]}`);
+// }
+
+// for (let i of subreddits) {
+//     console.log(i);
+// }
+
+// for (let row of seatingChart) {
+//     for (let stud of row) {
+//         console.log(stud);
+//     }
+// }
+
+for (let char of "hello world") {
+    console.log(char);
+}

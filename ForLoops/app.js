ca35fd1d9bd08b7797fe3895b81338294b82f085
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-01-19 14:03:34 +0900

    Nested loops

--- ForLoops/app.js
+++ ForLoops/app.js
@@ -9,3 +9,22 @@
 // for (let i = 100; i >= 0; i -= 10) {
 //     console.log(i);
 // }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`i is: ${i}`);
+//     for (let j = 1; j < 4; j++) {
+//         console.log(`   j is: ${j}`);
+//     }
+// }
+
+const seatingChart = [
+    ["Kristen", "Erik", "Namita"],
+    ["Geoffrey", "Juanita", "Antonio", "Kevin"],
+    ["Yuma", "Sakura", "Jack", "Erika"],
+];
+
+for (let i = 0; i < seatingChart.length; i++) {
+    for (let j = 0; j < seatingChart[i].length; j++) {
+        console.log(seatingChart[i][j]);
+    }
+}

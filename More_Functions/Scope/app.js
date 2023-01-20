c751cb91a39d283a2d761677355d247292acfe1b
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-01-20 17:59:26 +0900

    Lexical scope

--- More_Functions/Scope/app.js
+++ More_Functions/Scope/app.js
@@ -12,25 +12,35 @@
 //     console.log(bird);
 // }
 
-let radius = 0;
-if (radius > 0) {
-    const PI = 3.14159;
-    let msg = "HIII!";
-}
-console.log(radius);
-console.log(msg);
-for (var i = 0; i < 5; i++) {
-    var msg = "adkfjsldfj";
-    console.log(msg);
-}
-console.log(msg);
-console.log(i);
+// let radius = 0;
+// if (radius > 0) {
+//     const PI = 3.14159;
+//     let msg = "HIII!";
+// }
+// console.log(radius);
+// console.log(msg);
+// for (var i = 0; i < 5; i++) {
+//     var msg = "adkfjsldfj";
+//     console.log(msg);
+// }
+// console.log(msg);
+// console.log(i);
 
-let radius = 8;
-if (radius > 0) {
-    const PI = 3.14;
-    let circ = 2 * PI * radius;
+// let radius = 8;
+// if (radius > 0) {
+//     const PI = 3.14;
+//     let circ = 2 * PI * radius;
+// }
+// console.log(radius);
+// console.log(PI);
+// console.log(circ);
+
+function bankRobbery() {
+    const heros = ["Spiderman", "Wolverine", "Black Panther", "Batwoman"];
+    function cryForHelp() {
+        for (let hero of heros) {
+            console.log(`PLEASE HELP US, ${hero.toUpperCase()}`);
+        }
+    }
+    cryForHelp();
 }
-console.log(radius);
-console.log(PI);
-console.log(circ);

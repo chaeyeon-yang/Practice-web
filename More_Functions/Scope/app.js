fef23c73a74ed9daf0d56af0e3e20063b1bd49d3
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-01-20 09:43:11 +0900

    Block Scope

--- More_Functions/Scope/app.js
+++ More_Functions/Scope/app.js
@@ -1,13 +1,36 @@
-let totalEggs = 0;
-function collectEggs() {
-    totalEggs = 6;
-}
-console.log(totalEggs);
-collectEggs();
-console.log(totalEggs);
+// let totalEggs = 0;
+// function collectEggs() {
+//     totalEggs = 6;
+// }
+// console.log(totalEggs);
+// collectEggs();
+// console.log(totalEggs);
 
-let bird = "Scarlet Macaw";
-function birdWatch() {
-    let bird = "Great Blue Heron";
-    console.log(bird);
+// let bird = "Scarlet Macaw";
+// function birdWatch() {
+//     let bird = "Great Blue Heron";
+//     console.log(bird);
+// }
+
+let radius = 0;
+if (radius > 0) {
+    const PI = 3.14159;
+    let msg = "HIII!";
 }
+console.log(radius);
+console.log(msg);
+for (var i = 0; i < 5; i++) {
+    var msg = "adkfjsldfj";
+    console.log(msg);
+}
+console.log(msg);
+console.log(i);
+
+let radius = 8;
+if (radius > 0) {
+    const PI = 3.14;
+    let circ = 2 * PI * radius;
+}
+console.log(radius);
+console.log(PI);
+console.log(circ);

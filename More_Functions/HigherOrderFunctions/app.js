e74adf94123379d890e27b6e2d126147db3c0a17
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-01-21 21:18:05 +0900

    Returning functions

--- More_Functions/HigherOrderFunctions/app.js
+++ More_Functions/HigherOrderFunctions/app.js
@@ -1,16 +1,57 @@
-function callTwice(func) {
-    func();
-    func();
-}
+// function callTwice(func) {
+//     func();
+//     func();
+// }
 
-function callTenTimes(f) {
+// function callTenTimes(f) {
+//     for (let i = 0; i < 10; i++) {
+//         f();
+//     }
+// }
+// function rollDie() {
+//     const roll = Math.floor(Math.random() * 6) + 1;
+//     console.log(roll);
+// }
+
+// callTwice(rollDie);
+
     for (let i = 0; i < 10; i++) {
-        f();
+function makeMysteryFunc() {
+    const rand = Math.random();
+    if (rand > 0.5) {
+        return function () {
+            console.log("CONGRATS, I AM A GOOD FUNCTION!");
+            console.log("YOU WIN A MILLION DOLLARS!!");
+        };
+    } else {
+        return function () {
+            alert("YOU HAVE BEEN INFECTED BY A COMPUTER VIRUS!");
+            alert("STOP TRYING TO CLOSE THIS WINDOW!");
+            alert("STOP TRYING TO CLOSE THIS WINDOW!");
+            alert("STOP TRYING TO CLOSE THIS WINDOW!");
+            alert("STOP TRYING TO CLOSE THIS WINDOW!");
+            alert("STOP TRYING TO CLOSE THIS WINDOW!");
+            alert("STOP TRYING TO CLOSE THIS WINDOW!");
+        };
     }
 }
-function rollDie() {
-    const roll = Math.floor(Math.random() * 6) + 1;
-    console.log(roll);
-}
 
-callTwice(rollDie);
+// const mystery = makeMysteryFunc();
+
+// mystery();
+
+// function makeBetweenFunc(min, max) {}
+// makeBetweenFunc(50, 100);
+
+// function isBetween(num) {
+//     return num >= 50 && num <= 100;
+// }
+// function isBetween2(num) {
+//     return num >= 1 && num <= 10;
+// }
+
+function makeBetweenFunc(min, max) {
+    return function (num) {
+        return num >= min && num <= max;
+    };
+}

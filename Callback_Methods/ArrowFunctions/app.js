9c819974621bc63b30109be6b7c580a07eabd039
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-01-26 13:41:56 +0900

    Arrow Function Implicit Returns

--- Callback_Methods/ArrowFunctions/app.js
+++ Callback_Methods/ArrowFunctions/app.js
@@ -14,6 +14,13 @@ const square = (x) => {
     return x * x;
 };
 
-const rollDie = () => {
-    return Math.floor(Math.random() * 6) + 1;
-};
+// const rollDie = () => {
+//     return Math.floor(Math.random() * 6) + 1;
+// };
+
+// 함수의 중괄호를 소괄호 ()로 바꾸면 임시적 반환을 할 수 있다.
+// 괄호를 없애고 한 줄로 써도 무방하다.
+
+const rollDie = () => Math.floor(Math.random() * 6) + 1;
+
+const sub = (a, b) => a - b;

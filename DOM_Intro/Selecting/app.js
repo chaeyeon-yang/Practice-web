a9038fb1391f62625c77a15e2b9d455477857cd5
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-01-30 23:13:39 +0900

    querySelector & querySelectorAll

--- DOM_Intro/Selecting/app.js
+++ DOM_Intro/Selecting/app.js
@@ -10,3 +10,12 @@ for (let img of squareImages) {
     img.src =
         "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg";
 }
+
+// querySelector는 첫 번째 일치 항목을 제공한다.
+// querySelectorAll는 일치하는 모든 항목을 제공한다.
+
+const links = document.querySelectorAll("p a");
+
+for (let link of links) {
+    console.log(link.href);
+}

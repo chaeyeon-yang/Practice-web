56dfca5b42e70a56708945be664ab564e3674c98
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-01-30 23:16:16 +0900

    innerHTML, innerText & textContent

--- DOM_Intro/Manipulating/app.js
+++ DOM_Intro/Manipulating/app.js
@@ -3,3 +3,7 @@ const allLinks = document.querySelectorAll("a");
 for (let link of allLinks) {
     link.innerText = "I AM A LINK!!!";
 }
+
+document.querySelector("h1").innerHTML = "<i>adsfd</i>";
+document.querySelector("h1").innerHTML += "<sup>ddd</sup>";
+document.querySelector("p").innerText = "dsfsdf";

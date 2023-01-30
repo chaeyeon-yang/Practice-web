f7589fb419d20e6147c945e59df5408a129e2772
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-01-30 23:31:39 +0900

    Attributes

--- DOM_Intro/Manipulating/app.js
+++ DOM_Intro/Manipulating/app.js
@@ -7,3 +7,8 @@ for (let link of allLinks) {
 document.querySelector("h1").innerHTML = "<i>adsfd</i>";
 document.querySelector("h1").innerHTML += "<sup>ddd</sup>";
 document.querySelector("p").innerText = "dsfsdf";
+
+// 두 설정 방식에는 약간의 차이가 있다.
+const input = document.querySelector("input");
+input.setAttribute("type", "color");
+input.type = "color";

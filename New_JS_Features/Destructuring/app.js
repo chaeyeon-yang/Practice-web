bcd278a0afe760b58c834ffccc0e8df1185a30ab
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-01-30 21:51:57 +0900

    Destructuring Params

--- New_JS_Features/Destructuring/app.js
+++ New_JS_Features/Destructuring/app.js
@@ -29,7 +29,7 @@ const user = {
 
 // 두 구문은 동일하게 동작한다.
 // const email = user.email;
-const { email } = user; // email 속성 아래에 사용자로부터 email이라는 변수를 만드십시오.
+// const { email } = user; // email 속성 아래에 사용자로부터 email이라는 변수를 만드십시오.
 
 // 자체 변수로 분류하기 -> 원본 객체는 변경하지 않음
 const { email, firstName, lastName, city, bio } = user;
@@ -48,4 +48,75 @@ const user2 = {
     state: "Buk-gu",
 };
 
-const { city, state, died = "N/A" } = user2;
+// const { city, state, died = "N/A" } = user2;
+
+// function fullName(user) {
+//     return `${user.firstName} ${user.lastName}`;
+// }
+
+// function fullName(user) {
+//     const { firstName, lastName } = user;
+//     return `${firstName} ${lastName}`;
+// }
+
+function fullName({ firstName, lastName }) {
+    return `${firstName} ${lastName}`;
+}
+
+const movies = [
+    {
+        title: "Amadeus",
+        score: 99,
+        year: 1984,
+    },
+    {
+        title: "Sharknado",
+        score: 35,
+        year: 2013,
+    },
+    {
+        title: "13 Going on 30",
+        score: 70,
+        year: 2004,
+    },
+    {
+        title: "Stand By Me",
+        score: 85,
+        year: 1986,
+    },
+    {
+        title: "Waterworld",
+        score: 62,
+        year: 1995,
+    },
+    {
+        title: "Jingle All The Way",
+        score: 71,
+        year: 1996,
+    },
+    {
+        title: "Parasite",
+        score: 95,
+        year: 2019,
+    },
+    {
+        title: "Notting Hill",
+        score: 77,
+        year: 1999,
+    },
+    {
+        title: "Alien",
+        score: 90,
+        year: 1979,
+    },
+];
+
+movies.filter((movie) => movie.score >= 90);
+movies.filter(({ score }) => score >= 90);
+
+movies.map((movie) => {
+    return `${movie.title} (${movie.year}) is rated ${movie.score}`;
+});
+movies.map(({ title, year, score }) => {
+    return `${title} (${year}) is rated ${score}`;
+});

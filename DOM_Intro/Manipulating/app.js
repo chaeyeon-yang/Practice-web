14e00207d8d434467b7e3acac45092f18fe4a9ab
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-01-31 13:23:15 +0900

    Changing Styles

--- DOM_Intro/Manipulating/app.js
+++ DOM_Intro/Manipulating/app.js
@@ -1,8 +1,8 @@
 const allLinks = document.querySelectorAll("a");
 
-for (let link of allLinks) {
-    link.innerText = "I AM A LINK!!!";
-}
+// for (let link of allLinks) {
+//     link.innerText = "I AM A LINK!!!";
+// }
 
 document.querySelector("h1").innerHTML = "<i>adsfd</i>";
 document.querySelector("h1").innerHTML += "<sup>ddd</sup>";
@@ -12,3 +12,8 @@ document.querySelector("p").innerText = "dsfsdf";
 const input = document.querySelector("input");
 input.setAttribute("type", "color");
 input.type = "color";
+for (let link of allLinks) {
+    link.style.color = "rgb(0,108,134)";
+    link.style.textDecorationColor = "magenta";
+    link.style.textDecorationStyle = "wavy";
+}

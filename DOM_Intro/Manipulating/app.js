478b9506b84762dc6ac1896768d91240edf0a7fa
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-01-31 13:36:51 +0900

    ClassList

--- DOM_Intro/Manipulating/app.js
+++ DOM_Intro/Manipulating/app.js
@@ -17,3 +17,21 @@ for (let link of allLinks) {
     link.style.textDecorationColor = "magenta";
     link.style.textDecorationStyle = "wavy";
 }
+
+const h2 = document.querySelector("h2");
+h2.setAttribute("class", "purple");
+h2.setAttribute("class", "border");
+
+let currentClasses = h2.getAttribute("class");
+
+// css 에서 하나 이상의 클래스를 갖는 방법
+h2.setAttribute("class", `${currentClasses} purple`);
+
+h2.classList;
+h2.classList.add("purple");
+h2.classList.remove("border");
+h2.classList.contains("border");
+
+h2.classList.toggle("puple");
+h2.classList.toggle("puple");
+h2.classList.toggle("puple");

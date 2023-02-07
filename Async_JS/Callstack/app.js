efe14cc70ae6710650f8da92367b1ae564ffc63b
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-02-07 18:45:31 +0900

    WebAPIs & Single Threaded

--- Async_JS/Callstack/app.js
+++ Async_JS/Callstack/app.js
@@ -5,3 +5,9 @@ const square = (x) => multiply(x, x);
 const isRightTriangle = (a, b, c) => {
     square(a) + square(b) === square(c);
 };
+
+console.log("Sending request to server!");
+setTimeout(() => {
+    console.log("Here is your data from the server...");
+}, 3000);
+console.log("I AM AT THE END OF THE FILE!!");

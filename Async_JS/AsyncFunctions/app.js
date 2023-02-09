fff2a430daefcda08dd807381c88d4dd403b42b8
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-02-09 16:28:45 +0900

    Handling Errors In Async Functions

--- Async_JS/AsyncFunctions/app.js
+++ Async_JS/AsyncFunctions/app.js
@@ -84,6 +84,13 @@ const fakeRequest = (url) => {
 };
 
 async function makeTwoRequest() {
-    let data1 = await fakeRequest("/page1");
-    console.log(data1);
+    try {
+        let data1 = await fakeRequest("/page1");
+        console.log(data1);
+        let data2 = await fakeRequest("/page2");
+        console.log(data2);
+    } catch (e) {
+        console.log("CAUGHT AAN ERROR!");
+        console.log("error is:", e);
+    }
 }

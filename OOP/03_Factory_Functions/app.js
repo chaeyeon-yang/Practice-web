dc8e4b392e0563c4c53d7d938dbf9731e27b8f52
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-02-13 16:10:32 +0900

    change file structure

--- OOP/03_Factory_Functions/app.js
+++ OOP/03_Factory_Functions/app.js
@@ -1,60 +1,36 @@
-// // RGB 색을 16진법으로 변환한다.
-// function hex(r, g, b, a) {
-//     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-// }
-// function rgb(r, g, b) {
-//     return `rgb(${r}, ${b}, ${g})`;
-// }
-
-// // hex(255, 100, 25);
-// // rgb(255, 100, 25);
-
-// function makeColor(r, g, b) {
-//     const color = {};
-//     color.r = r;
-//     color.g = g;
-//     color.b = b;
-//     color.rgb = function () {
-//         const { r, g, b } = this;
-//         return `rgb(${r}, ${g}, ${b})`;
-//     };
-//     color.hex = function () {
-//         const { r, g, b } = this;
-//         return (
-//             "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
-//         );
-//     };
-//     return color;
-// }
-
-// const firstColor = makeColor(35, 255, 150);
-// firstColor.rgb();
-// firstColor.hex();
-
-// const black = makeColor(0, 0, 0);
-// black.rgb();
-// black.hex();
-
-function Color(r, g, b) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
+// RGB 색을 16진법으로 변환한다.
+function hex(r, g, b, a) {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+function rgb(r, g, b) {
+    return `rgb(${r}, ${b}, ${g})`;
 }
 
-Color.prototype.rgb = function () {
-    const { r, g, b } = this;
-    return `rgb(${r}, ${g}, ${b})`;
-};
+// hex(255, 100, 25);
+// rgb(255, 100, 25);
 
-Color.prototype.hex = function () {
-    const { r, g, b } = this;
-    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-};
+function makeColor(r, g, b) {
+    const color = {};
+    color.r = r;
+    color.g = g;
+    color.b = b;
+    color.rgb = function () {
+        const { r, g, b } = this;
+        return `rgb(${r}, ${g}, ${b})`;
+    };
+    color.hex = function () {
+        const { r, g, b } = this;
+        return (
+            "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+        );
+    };
+    return color;
+}
 
-Color.prototype.rgba = function (a = 1.0) {
-    const { r, g, b } = this;
-    return `rgba(${r}, ${g}, ${b}, ${a})`;
-};
+const firstColor = makeColor(35, 255, 150);
+firstColor.rgb();
+firstColor.hex();
 
-const color1 = new Color(40, 50, 60);
-const color2 = new Color(0, 0, 0);
+const black = makeColor(0, 0, 0);
+black.rgb();
+black.hex();

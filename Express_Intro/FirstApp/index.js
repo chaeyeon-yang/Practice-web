516aea1c2f38d07696ac86b04c25c6f7269ca0ea
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-02-20 18:42:36 +0900

    The Request & Response Objects

--- Express_Intro/FirstApp/index.js
+++ Express_Intro/FirstApp/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 // console.dir(app);
 
-app.use(() => {
+app.use((req, res) => {
     console.log("WE GOT A NEW REQUEST!!!");
+    // console.dir(req);
+    res.send("<h1>This is my webpage!</h1>");
 });
 
 app.listen(8080, () => {

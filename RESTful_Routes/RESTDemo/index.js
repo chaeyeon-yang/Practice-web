da493e5ac6f4045d447069d0b4b71f969bdf347b
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-02-22 16:07:59 +0900

    Parsing The Request Body

--- RESTful_Routes/RESTDemo/index.js
+++ RESTful_Routes/RESTDemo/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 
+// url encoded parse
+app.use(express.urlencoded({ extended: true }));
+// json payload parse
+app.use(express.json());
+
 app.get("/tacos", (req, res) => {
     res.send("GET /tacos response");
 });
 app.post("/tacos", (req, res) => {
-    res.send("POST /tacos response");
+    const { meat, qty } = req.body;
+    res.send(`OK, here are your ${qty} ${meat} tacos`);
 });
 
 app.listen(3000, () => {

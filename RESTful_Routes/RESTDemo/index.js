874632b48b3f0f9bb20cdf56e54cbca75f1c486c
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-03-02 14:27:55 +0900

    RESTful Comments Delete

--- RESTful_Routes/RESTDemo/index.js
+++ RESTful_Routes/RESTDemo/index.js
@@ -4,15 +4,18 @@ const path = require("path");
 const methodOverride = require("method-override");
 const { v4: uuid } = require("uuid"); // generating ID's
 
-// url encoded parse
+// url encoded parse, To parse form data in POST request body
 app.use(express.urlencoded({ extended: true }));
-// json payload parse
+// json payload parse, To parse incoming JSON in POST request body
 app.use(express.json());
+// To 'fake' put/patch/delete requests
 app.use(methodOverride("_method"));
+// Views folder and EJS setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-const comments = [
+// fake database
+let comments = [
     {
         id: uuid(),
         username: "chaeyeon",
@@ -34,45 +37,70 @@ const comments = [
         comment: "woof woof woof",
     },
 ];
-
+// **********************************
+// INDEX - renders multiple comments
+// **********************************
 app.get("/comments", (req, res) => {
     res.render("comments/index", { comments });
 });
-
+// **********************************
+// NEW - renders a form
+// **********************************
 app.get("/comments/new", (req, res) => {
     res.render("comments/new");
 });
-
+// **********************************
+// CREATE - creates a new comment
+// **********************************
 app.post("/comments", (req, res) => {
     const { username, comment } = req.body;
     comments.push({ username, comment, id: uuid() });
     res.redirect("/comments");
 });
-
+// *******************************************
+// SHOW - details about one particular comment
+// *******************************************
 app.get("/comments/:id", (req, res) => {
     const { id } = req.params;
     const comment = comments.find((c) => c.id === id);
     res.render("comments/show", { comment });
 });
-
+// *******************************************
+// EDIT - renders a form to edit a comment
+// *******************************************
 app.get("/comments/:id/edit", (req, res) => {
     const { id } = req.params;
     const comment = comments.find((c) => c.id === id);
     res.render("comments/edit", { comment });
 });
-
-// UPDATE
+// *******************************************
+// UPDATE - updates a particular comment
+// *******************************************
 app.patch("/comments/:id", (req, res) => {
     const { id } = req.params;
-    const newCommentText = req.body.comment;
     const foundComment = comments.find((c) => c.id === id);
+
+    // get new text from req.body
+    const newCommentText = req.body.comment;
+    // update the comment with the data from req.body
     foundComment.comment = newCommentText;
+    // redirect back to index
+    res.redirect("/comments");
+});
+
+// *******************************************
+// DELETE/DESTROY- removes a single comment
+// *******************************************
+app.delete("/comments/:id", (req, res) => {
+    const { id } = req.params;
+    comments = comments.filter((c) => c.id !== id);
     res.redirect("/comments");
 });
 
 app.get("/tacos", (req, res) => {
     res.send("GET /tacos response");
 });
+
 app.post("/tacos", (req, res) => {
     const { meat, qty } = req.body;
     res.send(`OK, here are your ${qty} ${meat} tacos`);

f9e5459cc12ddc335b30c01459a2ccf2a20fc517
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-03-07 14:36:51 +0900

    Refactoring Code

--- MongooseBasics/index.js
+++ MongooseBasics/index.js
@@ -1,9 +1,11 @@
 // mongoose를 MongoDB 서버에 연결해야한다.
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/movieApp");
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-    console.log("Connected successfully to MongoDB server");
-});
+mongoose
+    .connect("mongodb://127.0.0.1:270232417/movieApp")
+    .then(() => {
+        console.log("CONNECT OPEN!!!");
+    })
+    .catch((err) => {
+        console.log("OH NO ERROR!!!!");
+        console.log(err);
+    });

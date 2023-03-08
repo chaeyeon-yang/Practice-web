1b83d4866372b606e4e4f9aecef78bc18bfb13a4
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-03-08 23:41:20 +0900

    Validating Mongoose Updates

--- MongooseBasics/product.js
+++ MongooseBasics/product.js
@@ -42,11 +42,27 @@ const Product = mongoose.model("Product", productSchema);
 // 스키마에 정의되지 않은 추가 정보를 담아도 오류가 나지 않는다.
 // 하지만 데이터베이스에 저장되지 않는다.
 const bike = new Product({
-    name: "Bike Helmet",
+    name: "Tire Pump",
     price: 19.5,
-    categories: ["Cycling", "Safety"],
+    categories: ["Cycling"],
 });
-bike.save()
+// bike.save()
+//     .then((data) => {
+//         console.log("IT WORKED!");
+//         console.log(data);
+//     })
+//     .catch((err) => {
+//         console.log("OH NO ERROR!");
+//         console.log(err);
+//     });
+
+// 업데이트를 진행할 때 초기 설정해놓은 스키마의 유효성 검사가 무시된다.
+// 따라서 옵션으로 설정한 후 update 해줘야 유효성이 안무너진다.
+Product.findOneAndUpdate(
+    { name: "Tire Pump" },
+    { price: -9.99 },
+    { new: true, runValidators: true }
+)
     .then((data) => {
         console.log("IT WORKED!");
         console.log(data);

e9955063ff61c6548d4c93bb342091a31fa4ae5c
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-03-08 23:53:48 +0900

    Mongoose Validation Errors

--- MongooseBasics/product.js
+++ MongooseBasics/product.js
@@ -18,7 +18,7 @@ const productSchema = new mongoose.Schema({
     price: {
         type: Number,
         required: true,
-        min: 0,
+        min: [0, "Price must be positive ya dodo!"],
     },
     onSale: {
         type: Boolean,
@@ -35,6 +35,10 @@ const productSchema = new mongoose.Schema({
             default: 0,
         },
     },
+    size: {
+        type: String,
+        enum: ["S", "M", "L"],
+    },
 });
 
 const Product = mongoose.model("Product", productSchema);
@@ -42,27 +46,12 @@ const Product = mongoose.model("Product", productSchema);
 // 스키마에 정의되지 않은 추가 정보를 담아도 오류가 나지 않는다.
 // 하지만 데이터베이스에 저장되지 않는다.
 const bike = new Product({
-    name: "Tire Pump",
-    price: 19.5,
+    name: "Cycling Jersey",
+    price: 28.5,
     categories: ["Cycling"],
+    size: "XS",
 });
-// bike.save()
-//     .then((data) => {
-//         console.log("IT WORKED!");
-//         console.log(data);
-//     })
-//     .catch((err) => {
-//         console.log("OH NO ERROR!");
-//         console.log(err);
-//     });
-
-// 업데이트를 진행할 때 초기 설정해놓은 스키마의 유효성 검사가 무시된다.
-// 따라서 옵션으로 설정한 후 update 해줘야 유효성이 안무너진다.
-Product.findOneAndUpdate(
-    { name: "Tire Pump" },
-    { price: -9.99 },
-    { new: true, runValidators: true }
-)
+bike.save()
     .then((data) => {
         console.log("IT WORKED!");
         console.log(data);
@@ -71,3 +60,19 @@ Product.findOneAndUpdate(
         console.log("OH NO ERROR!");
         console.log(err);
     });
+
+// 업데이트를 진행할 때 초기 설정해놓은 스키마의 유효성 검사가 무시된다.
+// 따라서 옵션으로 설정한 후 update 해줘야 유효성이 안무너진다.
+// Product.findOneAndUpdate(
+//     { name: "Tire Pump" },
+//     { price: -9.99 },
+//     { new: true, runValidators: true }
+// )
+//     .then((data) => {
+//         console.log("IT WORKED!");
+//         console.log(data);
+//     })
+//     .catch((err) => {
+//         console.log("OH NO ERROR!");
+//         console.log(err);
+//     });

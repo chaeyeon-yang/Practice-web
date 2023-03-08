128a5beab466f46261cc199f3aadd0b94c3a7f6e
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-03-08 23:31:08 +0900

    Additional Schema Constraints

--- MongooseBasics/product.js
+++ MongooseBasics/product.js
@@ -13,10 +13,27 @@ const productSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
+        maxLength: 20,
     },
     price: {
         type: Number,
         required: true,
+        min: 0,
+    },
+    onSale: {
+        type: Boolean,
+        default: false,
+    },
+    categories: [String],
+    qty: {
+        online: {
+            type: Number,
+            default: 0,
+        },
+        inStore: {
+            type: Number,
+            default: 0,
+        },
     },
 });
 
@@ -24,7 +41,11 @@ const Product = mongoose.model("Product", productSchema);
 
 // 스키마에 정의되지 않은 추가 정보를 담아도 오류가 나지 않는다.
 // 하지만 데이터베이스에 저장되지 않는다.
-const bike = new Product({ name: "Mountain Bike", price: 999, color: "red" });
+const bike = new Product({
+    name: "Bike Helmet",
+    price: 19.5,
+    categories: ["Cycling", "Safety"],
+});
 bike.save()
     .then((data) => {
         console.log("IT WORKED!");
